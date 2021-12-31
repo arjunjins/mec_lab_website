9c2076c965e163f742b265626517637e0b1bbf07
Aromal <72349479+AromalPradeep@users.noreply.github.com>
2021-12-31 09:25:34 +0530

    Add files via upload

--- Project_Keyword (Mec labs 2021)/test2.py
+++ Project_Keyword (Mec labs 2021)/test2.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template
 import webbrowser
 
-app = Flask(__name__)
+import random as r
 
+app = Flask(__name__)
+'''
 @app.route('/')
 def my_form():
     return render_template('my-form.html')
@@ -13,6 +15,15 @@ def my_form_post():
     processed_text = text.upper()
     return render_template('my-form.html',y=processed_text)
     return processed_text
+'''
+@app.route('/')
+def my_form():
+    return render_template('test.html',word=word(),word2="dream")
+
+@app.route("/word")
+def word():
+    print("clicked")
+    return str(r.randint(0,100))
 
 if __name__ == '__main__':
     webbrowser.open_new_tab('http://localhost:5000/')
